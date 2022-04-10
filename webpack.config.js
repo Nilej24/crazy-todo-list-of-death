5b661b681c82ffee4722bdfbf3fb632d58d8c1b7
Nijel Manalo <nmanalo543@gmail.com>
2022-04-10 23:57:46 +0100

    set up bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,53 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
+    assetModuleFilename: "[name][ext]",
   },
   mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: "./dist",
+    },
+    open: true,
+    compress: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
+                },
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
